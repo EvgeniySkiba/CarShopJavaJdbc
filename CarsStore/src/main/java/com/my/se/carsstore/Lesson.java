dfbaac03d38a1d98965aeb5940c5f5826d91beb6
EvgeniySkiba <Skiba.Evgeniy@hotmail.com>
2017-04-28 16:11:06 +0400

    initial coommit

---
+++ CarsStore/src/main/java/com/my/se/carsstore/Lesson.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.my.se.carsstore;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
+import java.sql.ResultSet;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Skiba
+ */
+public class Lesson {
+
+    /// https://www.codeproject.com/Tips/815186/Java-JDBC-SQLite-Read-Data-from-User-selected-db-T
+    ///Java & JDBC & SQLite: Read Data from User-selected db Table and Show in JTable
+    
+    public static void main(String[] args) {
+
+        //4. CTRL+/ Toggle Comment
+        //5. ALT+SHIFT+F Format Code
+        //8. ALT+INSERT Generate Code
+        // ALT+F7 Find Usages
+        Connection connection = null; // храним соеденение с базой данных
+        Statement statement = null;// выполнение запросов
+        ResultSet resultSet = null;
+
+        System.out.println("HERE");
+        try {
+            // регистрируем драйвер
+            Driver d = (Driver) Class.forName("org.sqlite.JDBC").newInstance();
+
+            //создание и подключение к базе данных
+            String url = "jdbc:sqlite:G:\\PROJECT\\Leaarning\\Java\\CarsSaling\\dataBase\\CarShop.db3";
+            connection = DriverManager.getConnection(url);
+
+            String sql = "select * from CarModel";
+            statement = connection.createStatement();
+
+            // выполнение запроса 
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name_ru"));
+            }
+
+            // get all tables -->
+            System.out.println("************ Current table ***********");
+            ResultSet rs = null;
+            DatabaseMetaData meta = connection.getMetaData();
+            rs = meta.getTables(null, null, null, new String[]{"TABLE"});
+
+            while (rs.next()) {
+                String tableName = rs.getString("TABLE_NAME");
+                System.out.println(tableName);
+            }
+            
+             // get all tables <--
+
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
