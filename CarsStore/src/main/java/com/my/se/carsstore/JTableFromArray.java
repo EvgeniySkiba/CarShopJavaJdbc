a1c60bc7248fc84b8d7e27304757da91bbdd4dd1
EvgeniySkiba <Skiba.Evgeniy@hotmail.com>
2017-04-28 16:39:48 +0400

    added test class for test Jtable

---
+++ CarsStore/src/main/java/com/my/se/carsstore/JTableFromArray.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.my.se.carsstore;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author Skiba
+ */
+public class JTableFromArray {
+    
+    private static final  int HEIGHT = 200;
+    private static final int WEIGHT = 400;
+    public static void main(String [] args){
+        
+        // массив с названиями для таблицы 
+        Object [] columnHeader =  {"Имя", "Фамилия", "Отчество"};
+        
+        // массив с данными для таблицы 
+        Object [][] tableData = {
+            {"Иванов","Иван","Иваныч"},
+            {"Петров","Олег","Петрович"},
+            {"Aлексеев","Алексей","Алексеевич"},
+            {"Смирнов2","Павел","Яковлевич"},
+            {"Смирнов3","Павел","Яковлевич"},
+            {"Смирнов4","Павел","Яковлевич"},
+            {"Смирнов5","Павел","Яковлевич"},
+            {"Смирнов6","Павел","Яковлевич"},
+            {"Смирнов7","Павел","Яковлевич"},
+            {"Смирнов8","Павел","Яковлевич"},
+            {"Смирнов9","Павел","Яковлевич"}
+        };
+        
+        JFrame frame = new JFrame("JTableExample");
+        frame.getContentPane().setLayout(new FlowLayout());
+        
+        frame.setSize(400,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JTable jTableFio = new JTable(tableData,columnHeader);
+        JScrollPane scrol = new JScrollPane(jTableFio);
+       
+        
+        //размеры прокручиваемой области 
+        jTableFio.setPreferredScrollableViewportSize(new Dimension(400,200));
+        
+        frame.getContentPane().add(scrol);
+        frame.setVisible(true);        
+    }
+}
